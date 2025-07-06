2858e360aaf5058a405e1850727a73a7e83a5ea6
Michael Brooks <michaelbrooks@users.noreply.github.com>
2025-07-06 00:48:26 +0000

    fix response bug from model

--- custom_components/llmvision/providers.py
+++ custom_components/llmvision/providers.py
@@ -1061,6 +1061,7 @@ class Ollama(Provider):
 
         response = await self._post(url=endpoint, headers={}, data=data)
         response_text = response.get("message").get("content")
+        response_text = response_text.removesuffix("<end_")
         return response_text
 
     def _prepare_vision_data(self, call: dict) -> dict:
